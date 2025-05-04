b83be417c0d766240bb1d254c4a08eec0f66a3c7
786092 <Irfaan.S7860@gmail.com>
2025-05-04 12:50:52 -0700

    Starting LEagueInvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,14 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+	JFrame frame = new JFrame();
+	static final int WIDTH = 500;
+	static final int HEIGHT = 800;
+	public LeagueInvaders() {
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(WIDTH,HEIGHT);
+		
+		
+	}
 }
---
+++ src/LeagueInvadersRunner.java
@@ -0,0 +1,6 @@
+
+public class LeagueInvadersRunner {
+	public static void main(String[]args) {
+		
+	}
+}
