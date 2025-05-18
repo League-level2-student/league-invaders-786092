7ce45b28f229da5234ead9cf0cb58cdbb537c3b3
786092 <Irfaan.S7860@gmail.com>
2025-05-18 12:55:41 -0700

    Working on League Invaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,6 +1,7 @@
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
+	GamePanel panel = new GamePanel();
 	JFrame frame = new JFrame();
 	static final int WIDTH = 500;
 	static final int HEIGHT = 800;
@@ -8,7 +9,9 @@ public class LeagueInvaders {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 		frame.setSize(WIDTH,HEIGHT);
-		
+		frame.add(panel);
+		}
+	public void setup() {
 		
 	}
 }
--- src/LeagueInvadersRunner.java
+++ src/LeagueInvadersRunner.java
@@ -1,6 +1,7 @@
 
 public class LeagueInvadersRunner {
 	public static void main(String[]args) {
-		
+		LeagueInvaders invaders = new LeagueInvaders();
+		invaders.setup();
 	}
 }
